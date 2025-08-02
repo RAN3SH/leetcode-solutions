e0e7ed3f6cc09e432468eeae3c0304979c81f516
RaneshChandraKatta <katta.raneshchandra@gmail.com>
2025-08-02 16:54:31 +0530

    Delete Algorithms directory

--- Algorithms/1920. Build Array from Permutation.py
+++
@@ -1,8 +0,0 @@
-    def buildArray(self, nums: List[int]) -> List[int]:
-        a = []
-        for i, x in enumerate(nums):
-            a.append(nums[nums[i]])
-        return a
-
-    def buildArray2(self, nums: List[int]) -> List[int]:
-        return [nums[x] for x in nums]
