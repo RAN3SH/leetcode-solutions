3fe148a955bc635714b0bfa4fd152f83559ceac1
Ranesh Chandra Katta <katta.raneshchandra@gmail.com>
2025-08-06 16:23:53 +0530

    Update 387.First Unique Character in a String.py

--- algorithms/387.First Unique Character in a String.py
+++ algorithms/387.First Unique Character in a String.py
@@ -8,4 +8,12 @@ class Solution:
         for i, ch in enumerate(s):
             ch_i = ord(ch) - ord('a')
             if count[ch_i] == 0:
-                q.append((ch_i, i
+                q.append((ch_i, i))
+            count[ch_i] += 1
+
+        while q:
+            ch_i, idx = q[0]
+            if count[ch_i] == 1:
+                return idx
+            q.popleft()
+        return -1
