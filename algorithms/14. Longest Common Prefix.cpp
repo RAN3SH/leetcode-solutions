a3a28750c0c575bb6f9745b355da7782f45ab363
Ranesh Chandra Katta <katta.raneshchandra@gmail.com>
2025-09-19 19:48:13 +0530

    Create 14. Longest Common Prefix.cpp

---
+++ algorithms/14. Longest Common Prefix.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& words) {
+    string commonPrefixStr = "";
+    for (int i = 0; i < words[0].size(); i++) {
+        for (string& w : words) {
+            if (words[0][i] == w[i]) {
+                // good
+            } else {
+                return commonPrefixStr;
+            }
+        }
+        commonPrefixStr += words[0][i];
+    }
+    return commonPrefixStr;
+}
+
+string longestCommonPrefix2222222222(vector<string>& words) {
+    string commonPrefixStr = "";
+    string firstWord = words[0];
+    for (int i = 0; i < firstWord.size(); i++) {
+        char firstWordCharAti = firstWord[i];
+        for (string& w : words) {
+            char otherWordCharAti = w[i];
+            if (firstWordCharAti == otherWordCharAti) {
+                // good
+            } else {
+                return commonPrefixStr;
+            }
+        }
+        commonPrefixStr += firstWordCharAti;
+    }
+    return commonPrefixStr;
+}
