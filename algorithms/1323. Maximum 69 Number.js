4bebc9c2a0d4e53184ebd8d43dd33424f9332e97
Ranesh Chandra Katta <katta.ranesh.chandra@gmail.com>
2025-09-22 19:37:03 +0530

    Create 1323. Maximum 69 Number.js

---
+++ algorithms/1323. Maximum 69 Number.js
@@ -0,0 +1,16 @@
+var maximum69Number = function (num) {
+    let s = '' + num
+    let t = ''
+    let changed = 0
+    for (let d of s) {
+        if (!changed) {
+            if (d === '6') {
+                d = '9'
+                changed = 1
+            }
+        }
+        t += d
+    }
+    return +t
+};
+1
